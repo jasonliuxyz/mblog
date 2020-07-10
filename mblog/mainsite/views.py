cc064917cc7a1ed6a0a9f6e8012ccf326146853d
liucheng <jason.liu@ucloud.cn>
2020-07-10 18:34:01 +0800

    add template

--- mblog/mainsite/views.py
+++ mblog/mainsite/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.template.loader import get_template
+from django.http import HttpResponse
+from .models import Post
+from datetime import datetime
+from django.shortcuts import redirect
 
-# Create your views here.
+#def homepage(request):
+#    posts = Post.objects.all()
+#    post_lists = list()
+#    for count, post in enumerate(posts):
+#        post_lists.append("No.{}:".format(str(count)) + str(post)+"<br>")
+#        post_lists.append("<small>" + str(post.body.encode('utf-8')) + "</small><br><br>")
+#    return HttpResponse(post_lists)
+
+def homepage(request):
+    template = get_template('index.html')
+    posts = Post.objects.all()
+    now = datetime.now()
+    html = template.render(locals())
+    return HttpResponse(html)
+
+def showpost(request, slug):
+    template = get_template('post.html')
+    try:
+        post = Post.objects.get(slug=slug)
+        if post != None:
+            html = template.render(locals())
+            return HttpResponse(html)
+    except:
+        return redirect('/')
